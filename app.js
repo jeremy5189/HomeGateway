d34da54091edb246eae4815ba6fa131db8f27a19
Jeremy Yen <jeremy5189@gmail.com>
2016-09-14 23:38:50 +0800

    Fix Code

--- app.js
+++ app.js
@@ -1,117 +1,174 @@
-var express = require('express');
-var app = express();
+// ------------------
+// Init Express
+// ------------------
+var express  = require('express'),
+    app      = express(),
+    DOC_ROOT = '/home/pi/DoorServer',
+    PORT     = 3000;
 
-var doc_root = '/home/pi/DoorServer';
 
-// Init GPIO
-run_cmd( doc_root + '/scripts/init.sh', [], function(){});
-
-// Lock on Init
-run_cmd( doc_root + '/scripts/lock.sh', [], function(){});
-
-// Play Init Sound
-run_cmd('aplay', ['/home/pi/DoorServer/sound/init_lock.wav'], function(){});
-
+// ------------------
+// Index 
+// ------------------
 app.get('/', function (req, res) {
-  res.send('Hello Home Gateway Lock Server');
+
+  res.send('Home Gateway');
+
 });
 
+// ------------------
+// Get Record Photo
+// ------------------
 app.get('/photo/:event/:timestamp', function(req, res) {
 
-  var event 	= req.params.event,
+  var event 	  = req.params.event,
       timestamp = req.params.timestamp;
 
   console.log(event);
   console.log(timestamp);
 
-  var fs  = require('fs');
-  var img = fs.readFileSync(doc_root + '/photo/' + event + '/' + timestamp + '.jpg');
-  res.writeHead(200, {'Content-Type': 'image/jpeg' });
+  var fs   = require('fs'),
+      path = DOC_ROOT + '/photo/' + event + '/' + timestamp + '.jpg',
+      img  = fs.readFileSync(path);
+
+  res.writeHead(200, {'Content-Type': 'image/jpeg'});
   res.end(img, 'binary'); 
   
 });
 
+// ------------------------------------------------------
+// Take Photo (With option ?event=open to not return)
+// ------------------------------------------------------
 app.get('/photo', function(req, res) {
   
-  console.log('GET /photo ' + req.ip);
+    logging('GET /photo ip = ' + req.ip);
 
-  // If event == open then dont return photo in res
-  var event = null;
-  if( req.query.event != undefined )
-    event = req.query.event;
+    // If event == open then dont return photo in res
+    var event = null;
+    // ?event=
+    if( req.query.event != undefined )
+        event = req.query.event;
 
-  var fn, ts = (new Date().getTime());
+    var fn, ts = (new Date().getTime());
   
-  if( event == 'open' ) {
-    console.log('event == open');
-    fn = doc_root + '/photo/' + event + '/' + ts + '.jpg';
-  } else {
-    fn = '/tmp/' + ts + '.jpg';
-  }
+    if( event == 'open' ) {
+        logging('event = open');
+        fn = DOC_ROOT + '/photo/' + event + '/' + ts + '.jpg';
+    } 
+    else {
+        fn = '/tmp/' + ts + '.jpg';
+    }
 
-  console.log('fn = ' + fn);
-  run_cmd('raspistill', [
-    '-o',
-    fn,
-    '-w',
-    '800',
-    '-h',
-    '600',
-    '-hf', 
-    '-vf',
-    '-n'
-  ], function() {
+    logging('Photo fn = ' + fn);
+    run_cmd('raspistill', [
+        '-o',
+        fn,
+        '-w',
+        '800',
+        '-h',
+        '600',
+        '-hf', 
+        '-vf',
+        '-n'
+    ], function() {
 
-    console.log('Photo done');
+        logging('Photo done');
 
-	if(event == null) {
-	    	var fs  = require('fs');
-	    	var img = fs.readFileSync(fn);
-	    	res.writeHead(200, {'Content-Type': 'image/jpeg' });
-	    	res.end(img, 'binary'); 
+        if(event == null) {
+	    	    var fs  = require('fs');
+	    	    var img = fs.readFileSync(fn);
+	    	    res.writeHead(200, {'Content-Type': 'image/jpeg' });
+	    	    res.end(img, 'binary'); 
         }
   
-  });
-  res.send({name: ts});
-});
-
-app.get('/lock', function(req, res) {
-  console.log('GET /lock '+ req.ip);
-
-  run_cmd(doc_root + '/scripts/lock.sh', [], function(output) {
-    res.send({status: 1});
-  });
-});
-
-app.get('/unlock', function(req, res) {
-  console.log('GET /unlock ' + req.ip);
-  run_cmd(doc_root + '/scripts/unlock.sh', [], function(output) {
-    res.send({status: 0});
-  });
-});
-
-app.get('/status', function(req, res) {
-  console.log('GET /status ' + req.ip);
-
-  run_cmd(doc_root + '/scripts/status.sh', [], function(output) {
-    var ret = parseInt(output.replace("\n", '').replace(" ", ""));
-    console.log('status = ' + ret);
-    res.send({
-	lock: ret
     });
-  });
+
+    // Send back record photo name to let db save it first
+    if( event == 'open' )
+        res.send({name: ts});
 });
 
-app.listen(3000, function () {
-  console.log('DoorServer listening on port 3000');
+// ------------------
+// Lock 
+// ------------------
+app.get('/lock/lock', function(req, res) {
+  
+    logging('GET /lock ip = '+ req.ip);
+
+    run_cmd(DOC_ROOT + '/scripts/lock.sh', [], function(output) {
+        res.send({status: 1});
+    });
 });
 
-function run_cmd(cmd, args, callBack) {
-    var spawn = require('child_process').spawn;
-    var child = spawn(cmd, args);
-    var resp = "";
-    console.log(args);
-    child.stdout.on('data', function (buffer) { resp += buffer.toString() });
-    child.stdout.on('end', function() { callBack (resp) });
+// ------------------
+// Unlcok 
+// ------------------
+app.get('/lock/unlock', function(req, res) {
+
+    logging('GET /unlock ip = ' + req.ip);
+
+    run_cmd(DOC_ROOT + '/scripts/unlock.sh', [], function(output) {
+        res.send({status: 0});
+    });
+});
+
+// ------------------
+// Lock Status 
+// ------------------
+app.get('/lock/status', function(req, res) {
+  
+    logging('GET /status ip = ' + req.ip);
+
+    run_cmd(DOC_ROOT + '/scripts/status.sh', [], function(output) {
+        var ret = parseInt(output.replace("\n", '').replace(" ", ""));
+        logging('Lock Status = ' + ret);
+        res.send({ lock: ret });
+    });
+});
+
+// ------------------
+// Start Express 
+// ------------------
+app.listen(PORT, function () {
+    
+    logging('HomeGateway listening on port 3000');
+    init();
+
+});
+
+var init = function() {
+    
+    // Init GPIO
+    run_cmd( DOC_ROOT + '/scripts/init.sh', [], function(){});
+
+    // Lock on Init
+    run_cmd( DOC_ROOT + '/scripts/lock.sh', [], function(){});
+
+    // Play Init Sound
+    run_cmd('aplay', ['/home/pi/DoorServer/sound/init_lock.wav']);
 }
 
+var logging = function(str) {
+
+    var moment = require('moment'),
+        now    = moment().format("YYYY-MM-DD HH:mm:ss");
+
+    console.log('[%s] %s', now, str);
+}
+
+var run_cmd = function (cmd, args, callback) {
+    
+    var spawn = require('child_process').spawn,
+        child = spawn(cmd, args),
+        resp = "";
+
+    child.stdout.on('data', function (buffer) { 
+      resp += buffer.toString() 
+    });
+
+    child.stdout.on('end', function() { 
+      if(callback)
+        callback(resp) 
+    });
+
+}
