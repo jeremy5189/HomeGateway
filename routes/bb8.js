011e5ae5b3acf563f8bb08715c269e3895a825d1
Jeremy Yen <jeremy5189@gmail.com>
2016-10-25 21:25:41 +0800

    Add reconnect

--- routes/bb8.js
+++ routes/bb8.js
@@ -3,11 +3,12 @@
 //var bb8_address = 'e146c3a8db364a6c9ff2ae739401ddd8';
 var bb8_address = 'ec:98:74:81:1c:5e';
 
-var express    = require('express'),
-	router     = express.Router(),
-	sphero     = require("sphero"),
-    bb8        = sphero(bb8_address),
-    bb8_status = false;
+var express     = require('express'),
+    router      = express.Router(),
+    sphero      = require("sphero"),
+    bb8         = sphero(bb8_address),
+    bb8_status  = false,
+    ping_handle = null;
 
 function bb8_sound(num) {
 	global.run_cmd('mpg123', [ global.DOC_ROOT + '/bb8-sounds/bb8-' + num + '.mp3']);
@@ -17,7 +18,7 @@ global.events.on('tag_all_connected', bb8_init);
 
 function bb8_init() {
 
-	global.logging('tag_all_connected event invoked');
+	global.logging('bb8_init, tag_all_connected event invoked');
 	bb8.connect(bb8_connected);
 }
 
@@ -27,6 +28,47 @@ function bb8_connected() {
 	bb8_status = true;
 	global.events.emit('bb8_connected');
 	bb8_sound('26');
+	bb8.version(function(err, data) {
+       if (err) {
+         console.log("error: ", err);
+       } else {
+         console.log("data:");
+         console.log("  recv:", data.recv);
+         console.log("  mdl:", data.mdl);
+         console.log("  hw:", data.hw);
+         console.log("  msaVer:", data.msaVer);
+         console.log("  msaRev:", data.msaRev);
+         console.log("  bl:", data.bl);
+         console.log("  bas:", data.bas);
+         console.log("  macro:", data.macro);
+         console.log("  apiMaj:", data.apiMaj);
+         console.log("  apiMin:", data.apiMin);
+       }
+    });
+
+	// Ping 10 secs
+    ping_handle = setInterval(bb8_ping, 1000 * 10);
+}
+
+function bb8_ping() {
+	global.logging('bb8 ping');
+	
+	bb8.ping(function(err, data) {
+    	
+    	if(err)
+    		console.error(err);
+
+    	console.log(data);
+	});
+}
+
+function bb8_disconnect() {
+	bb8.disconnect(function() {
+		global.logging('bb8 disconnected');
+		bb8_status = false;
+		clearInterval(ping_handle);
+		global.events.emit('bb8_disconnected');
+	});
 }
 
 router.ws('/status', function(ws, req) {
@@ -37,6 +79,14 @@ router.ws('/status', function(ws, req) {
 	}), function(err){});
 
 	global.events.on('bb8_connected', function() {
+
+		ws.send(JSON.stringify({
+			status : bb8_status,
+			address: bb8_address
+		}), function(err){});
+	});
+
+	global.events.on('bb8_disconnected', function() {
 
 		ws.send(JSON.stringify({
 			status : bb8_status,
@@ -86,6 +136,16 @@ function bb8_calibrate_finish() {
 	bb8.finishCalibration();
 }
 
+function bb8_reconnect() {
+	
+	bb8_disconnect();
+
+	// Wait and reconnect again
+	setTimeout(function() {
+		bb8_init();
+	}, 3000);
+}
+
 function get_random_num() {
 	var n = Math.floor(Math.random() * 32) + 1;
 	if( n < 10 )
@@ -127,6 +187,9 @@ router.ws('/control', function(ws, req) {
 				break;
 			case 'calibrate_finish':
 				bb8_calibrate_finish();
+				break;
+			case 'reconnect':
+				bb8_reconnect();
 				break;
 			default:
 				break;
